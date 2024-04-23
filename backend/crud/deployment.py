2f0003b54e3c208c601f70154d72753a77e87151
RafayDev <imabdulrafayy@gmail.com>
2024-04-23 14:57:52 +0500

    password enviroment varibable

--- backend/crud/deployment.py
+++ backend/crud/deployment.py
@@ -35,14 +35,14 @@ STORAGES = {
 
 CONNECTION = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 CONNECTION_STR = {pair.split('=')[0]:pair.split('=')[1] for pair in CONNECTION.split(' ')}
-print("Parsed connection string:", CONNECTION_STR)
+DB_PASS = os.environ['DB_PASS']
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": CONNECTION_STR['dbname'],
         "HOST": CONNECTION_STR['host'],
         "USER": CONNECTION_STR['user'],
-        "PASSWORD": CONNECTION_STR['password'],
+        "PASSWORD": DB_PASS,
     }
 }
 
